9d3831a3c43312a422de16150c00d99c28065cba
Rob Morgan <rmorgan323@gmail.com>
2018-02-02 23:33:22 -0700

    add accordion tabs

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,24 @@
 document.addEventListener('click', function(event) {
-  if (event.target.className.includes('tab-title')) {
+  if (event.target.className.includes('acc-title')) {
     selectTab(event.target);
   }
 })
 
 function selectTab(target) {
-  const titleArray = [].slice.call(target.parentNode.children);
-  const tabNum = target.classList[0].slice(6,7);
-  
-  titleArray.forEach(child => {
-    child.classList.remove('tab-active');
+  const tabNum = target.classList[1].slice(6,7);
+
+  document.querySelectorAll('.acc-title').forEach(title => {
+    title.classList.remove('title-active');
   })
-  target.classList.add('tab-active');
-  document.querySelectorAll('.tabs').forEach(tab => {
-    tab.classList.remove('active');
+  target.classList.add('title-active');
+
+  document.querySelectorAll('.plus-minus').forEach(char => {
+    char.innerText = '+';
   })
-  document.querySelector(`.tab-${tabNum}`).classList.add('active')
+  document.querySelector(`.plus-minus-${tabNum}`).innerText = '-';
+
+  document.querySelectorAll('.acc-content').forEach(content => {
+    content.classList.remove('content-active');
+  })
+  document.querySelector(`.content-${tabNum}`).classList.add('content-active')
 }
